ad032fe50e4bb2d61e5b315edb92dd0804988afc
jhoanmartinez <dsa.jhoan@gmail.com>
2021-06-27 21:34:37 -0500

    serializer.errors

--- snippets/views.py
+++ snippets/views.py
@@ -5,22 +5,20 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from .models import Snippet
+from .serializers import SnippetSerializer
 
 @api_view(['GET', 'POST'])
 def snippet_list(request):
-    """
-    Get and add all the users
-    """
     if request.method == 'GET':
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
+        snnipets = Snippet.objects.all()
+        serializer = SnippetSerializer(snnipets, many=True)
         return Response(serializer.data)
     
     elif request.method == 'POST':
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
